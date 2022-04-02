796d2bd1c2f8ffa0361c44e3c0ca2b424c221d27
Michal Paukszto <michal.paukszto@gmail.com>
2022-04-02 09:38:17 +0200

    michal's commit

--- src/main/java/com/sda/weather/WeatherApplication.java
+++ src/main/java/com/sda/weather/WeatherApplication.java
@@ -3,6 +3,7 @@ package com.sda.weather;
 public class WeatherApplication {
 
     public static void main(String[] args) {
+        System.out.println("Michal tu był");
         System.out.println("Hello World");
     }
 }
